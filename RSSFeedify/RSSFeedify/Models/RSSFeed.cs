b2fec28308cbac4e93bca276e2bde6226fc0067f
Marek Eibel <536376@mail.muni.cz>
2024-05-07 13:24:31 +0200

    feat(db): add some basic model for rss-feed (+ testing tutorial models and autogenerated)

---
+++ RSSFeedify/RSSFeedify/Models/RSSFeed.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RSSFeedify.Models
+{
+    public class RSSFeed
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public Uri SourceUrl {  get; set; }
+        public double PollingInterval { get; set; }
+    }
+}
