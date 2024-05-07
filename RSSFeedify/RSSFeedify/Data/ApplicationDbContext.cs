a2ad25fd9156df9ecda5dae9bf1b729bd31b2176
Marek Eibel <536376@mail.muni.cz>
2024-05-07 13:25:31 +0200

    feat(db): add application db context

---
+++ RSSFeedify/RSSFeedify/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RSSFeedify.Models;
+
+namespace PostgreSQL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public DbSet<RSSFeed> RSSFeeds { get; set; }
+    }
+}
