ce2286c0c88f65e0a5406efe0a4949b2bca9ec6b
Marek Eibel <536376@mail.muni.cz>
2024-05-07 13:23:05 +0200

    feat(db): set up the database/web api and other configs

---
+++ RSSFeedify/RSSFeedify/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PostgreSQL.Data;
+using RSSFeedify.Models;
+using RSSFeedify.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Register your DbContext
+builder.Services.AddDbContext<ApplicationDbContext>();
+
+// Register IRSSFeedRepository implementation
+builder.Services.AddScoped<IRSSFeedRepository, RSSFeedRepository>(); // Assuming RSSFeedRepository is your implementation
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
