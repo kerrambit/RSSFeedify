384a64c0ab2730e8e60e96ec1dd940ea6327702b
Marek Eibel <536376@mail.muni.cz>
2024-05-07 19:47:46 +0200

    feat(db): add last polling times into RSSFeed model

--- RSSFeedify/RSSFeedify/Models/RSSFeed.cs
+++ RSSFeedify/RSSFeedify/Models/RSSFeed.cs
@@ -8,5 +8,7 @@ namespace RSSFeedify.Models
         public string? Description { get; set; }
         public Uri SourceUrl {  get; set; }
         public double PollingInterval { get; set; }
+        public DateTime LastPoll { get; set; }
+        public DateTime LastSuccessfullPoll { get; set; }
     }
 }
