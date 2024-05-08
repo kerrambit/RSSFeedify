5fbbd07eef13a6c4bdc67aa7e32f55d42fde1579
Marek Eibel <536376@mail.muni.cz>
2024-05-08 17:18:03 +0200

    refactor: refactor of huge method into smaller parts

--- RSSFeedify/RSSFeedify/Services/RSSFeedPollingService.cs
+++ RSSFeedify/RSSFeedify/Services/RSSFeedPollingService.cs
@@ -1,10 +1,8 @@
 ﻿using RSSFeedify.Models;
-using RSSFeedify.Repositories;
 using RSSFeedify.Repository;
 using RSSFeedify.Repository.Types;
 using RSSFeedify.Services.DataTypeConvertors;
 using System.ServiceModel.Syndication;
-using System.Threading.Channels;
 using System.Xml;
 
 namespace RSSFeedify.Services
@@ -24,7 +22,6 @@ namespace RSSFeedify.Services
             while (!stoppingToken.IsCancellationRequested)
             {
                 await Task.Delay(_pollingInterval, stoppingToken);
-
                 using (var scope = _scopeFactory.CreateScope())
                 {
                     Console.WriteLine("[RSSFeedPollingService]: service wakes up...");
@@ -39,83 +36,94 @@ namespace RSSFeedify.Services
 
                     } else
                     {
-                        // Parallel.ForEach(feeds.Data, feed =>
                         foreach (var feed in feeds.Data)
                         {
-                            bool readyToUpdate = false;
-                            Console.WriteLine($"\n\n{feed.Guid} - {feed.Name}, LastPoll: {feed.LastPoll}, LastSuccessfullPoll: {feed.LastSuccessfullPoll}, PollingInterval: {feed.PollingInterval}");
-                            if (feed.LastPoll == DateTime.MinValue && feed.LastSuccessfullPoll == DateTime.MinValue || feed.LastSuccessfullPoll != feed.LastPoll)
+                            if (!CheckTimeStampsForUpdate(feed))
                             {
-                                readyToUpdate = true;
-                                if (feed.LastPoll == DateTime.MinValue && feed.LastSuccessfullPoll == DateTime.MinValue)
-                                {
-                                    Console.WriteLine("[RSSFeedPollingService]: polling times were not initialized.");
-                                } else
-                                {
-                                    Console.WriteLine("[RSSFeedPollingService]: polling times are not equal. It seems that the last poll was not successfull.");
-                                }
-                            } else if (DateTime.UtcNow - feed.LastSuccessfullPoll >= TimeSpan.FromMinutes(feed.PollingInterval))
-                            {
-                                readyToUpdate = true;
-                                Console.WriteLine($"[RSSFeedPollingService]: last polling time is greater than the polling interval. See {DateTime.UtcNow - feed.LastSuccessfullPoll} >= {TimeSpan.FromMinutes(feed.PollingInterval)}.");
+                                Console.WriteLine($"[RSSFeedPollingService]: feed must be updated: {false}");
+                                continue;
                             }
 
-                            Console.WriteLine($"[RSSFeedPollingService]: feed must be updated: {readyToUpdate}");
-
-                            if (readyToUpdate)
+                            Console.WriteLine($"[RSSFeedPollingService]: feed must be updated: {true}");
+                            (bool success, DateTime lastUpdate, List<(string hash, RSSFeedItemDTO dto)> items) = LoadRSSFeedItemsFromUri(feed.SourceUrl);
+                            if (!success)
                             {
-                                (bool success, DateTime lastUpdate, List<(string hash, RSSFeedItemDTO dto)> items) = LoadRSSFeedItemsFromUri(feed.SourceUrl);
-                                if (success)
-                                {
-                                    if (feed.LastPoll <= lastUpdate)
-                                    {
-                                        Console.WriteLine($"[RSSFeedPollingService]: feed was updated so the database must be also updated ({feed.LastPoll} < {lastUpdate})");
-                                        
-                                        var originalItems = await rSSItemsRepository.GetAsyncFilteredByForeignKey(feed.Guid);
-                                        if (originalItems is not Success<IEnumerable<RSSFeedItem>>)
-                                        {
-                                            Console.WriteLine($"[RSSFeedPollingService]: failure. Unable to get access to RSSFeedsItems for feed: {feed.Guid}!");
-                                            await rSSRepository.UpdatePollingTimeAsync(feed.Guid, successfullPolling: false);
-                                        }
-                                        else
-                                        {
-                                            foreach (var newFeedItem in items)
-                                            {
-                                                bool isUnique = true;
-                                                foreach (var feedItem in originalItems.Data)
-                                                {
-                                                    if (newFeedItem.hash == feedItem.Hash)
-                                                    {
-                                                        isUnique = false;
-                                                        break;
-                                                    }
-                                                }
-                                                if (isUnique)
-                                                {
-                                                    var newRSSFeedItem = RSSFeedItemDTOToRssFeedItem.Convert(newFeedItem.dto, newFeedItem.hash);
-                                                    newRSSFeedItem.RSSFeedId = feed.Guid;
-                                                    _ = rSSItemsRepository.Insert(newRSSFeedItem);
-                                                    Console.WriteLine($"[RSSFeedPollingService]: new RSSFeedItem should be inserted {newRSSFeedItem.Title}.");
-                                                }
-                                            }
-                                            await rSSRepository.UpdatePollingTimeAsync(feed.Guid, successfullPolling: true);
-                                        }
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"[RSSFeedPollingService]: feed was not updated so our RSSFeed can be left as it is and only time stamps are going to be updated.");
-                                        await rSSRepository.UpdatePollingTimeAsync(feed.Guid, successfullPolling: true);
-                                    }
-                                } else
-                                {
-                                    Console.WriteLine($"[RSSFeedPollingService]: poll was not successfull so only LastPoll time stamp will be updated.");
-                                    await rSSRepository.UpdatePollingTimeAsync(feed.Guid, successfullPolling: false);
-                                }
+                                Console.WriteLine($"[RSSFeedPollingService]: poll was not successfull so only LastPoll time stamp will be updated.");
+                                await rSSRepository.UpdatePollingTimeAsync(feed.Guid, successfullPolling: false);
+                                continue;
                             }
-                        } //);
+                            
+                            if (feed.LastPoll > lastUpdate)
+                            {
+                                Console.WriteLine($"[RSSFeedPollingService]: feed was not updated so our RSSFeed can be left as it is and only time stamps are going to be updated.");
+                                await rSSRepository.UpdatePollingTimeAsync(feed.Guid, successfullPolling: true);
+                                continue;
+                            }
+
+                            Console.WriteLine($"[RSSFeedPollingService]: feed was updated so the database must be also updated ({feed.LastPoll} < {lastUpdate})");
+
+                            var originalItems = await rSSItemsRepository.GetAsyncFilteredByForeignKey(feed.Guid);
+                            if (originalItems is not Success<IEnumerable<RSSFeedItem>>)
+                            {
+                                Console.WriteLine($"[RSSFeedPollingService]: failure. Unable to get access to RSSFeedsItems for feed: {feed.Guid}!");
+                                await rSSRepository.UpdatePollingTimeAsync(feed.Guid, successfullPolling: false);
+                                continue;
+                            }
+
+                            UpdateRSSFeedItems(rSSItemsRepository, feed, items, originalItems);
+                            await rSSRepository.UpdatePollingTimeAsync(feed.Guid, successfullPolling: true);
+                        }
                     }
                 }
             }
+        }
+
+        private static void UpdateRSSFeedItems(IRSSFeedItemRepository rSSItemsRepository, RSSFeed feed, List<(string hash, RSSFeedItemDTO dto)> items, RepositoryResult<IEnumerable<RSSFeedItem>> originalItems)
+        {
+            foreach (var newFeedItem in items)
+            {
+                bool isUnique = true;
+                foreach (var feedItem in originalItems.Data)
+                {
+                    if (newFeedItem.hash == feedItem.Hash)
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique)
+                {
+                    var newRSSFeedItem = RSSFeedItemDTOToRssFeedItem.Convert(newFeedItem.dto, newFeedItem.hash);
+                    newRSSFeedItem.RSSFeedId = feed.Guid;
+                    _ = rSSItemsRepository.Insert(newRSSFeedItem);
+                    Console.WriteLine($"[RSSFeedPollingService]: new RSSFeedItem should be inserted {newRSSFeedItem.Title}.");
+                }
+            }
+        }
+
+        private static bool CheckTimeStampsForUpdate(RSSFeed feed)
+        {
+            bool readyToUpdate = false;
+            Console.WriteLine($"\n\n{feed.Guid} - {feed.Name}, LastPoll: {feed.LastPoll}, LastSuccessfullPoll: {feed.LastSuccessfullPoll}, PollingInterval: {feed.PollingInterval}");
+            if (feed.LastPoll == DateTime.MinValue && feed.LastSuccessfullPoll == DateTime.MinValue || feed.LastSuccessfullPoll != feed.LastPoll)
+            {
+                readyToUpdate = true;
+                if (feed.LastPoll == DateTime.MinValue && feed.LastSuccessfullPoll == DateTime.MinValue)
+                {
+                    Console.WriteLine("[RSSFeedPollingService]: polling times were not initialized.");
+                }
+                else
+                {
+                    Console.WriteLine("[RSSFeedPollingService]: polling times are not equal. It seems that the last poll was not successfull.");
+                }
+            }
+            else if (DateTime.UtcNow - feed.LastSuccessfullPoll >= TimeSpan.FromMinutes(feed.PollingInterval))
+            {
+                readyToUpdate = true;
+                Console.WriteLine($"[RSSFeedPollingService]: last polling time is greater than the polling interval. See {DateTime.UtcNow - feed.LastSuccessfullPoll} >= {TimeSpan.FromMinutes(feed.PollingInterval)}.");
+            }
+
+            return readyToUpdate;
         }
 
         public static string GenerateRSSFeedItemHash(string title, DateTime publishDate)
