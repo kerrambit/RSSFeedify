18462a0e7d30092d2a1c62b06372d8f4533b9db1
Marek Eibel <536376@mail.muni.cz>
2024-05-08 23:56:46 +0200

    feat(cli): implement endpoints class

---
+++ RSSFeedify/RSSFeedifyCLIClient/Settings/Endpoints.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSFeedifyCLIClient.Settings
+{
+    public static class Endpoints
+    {
+        public static Uri BaseUrl { get; set; } = new(@"https://localhost:32776/api/");
+
+        public enum EndPoint
+        {
+            RSSFeeds
+        };
+
+        public static Uri BuildUri(EndPoint endPoint)
+        {
+            UriBuilder uriBuilder = new UriBuilder(BaseUrl);
+
+            switch (endPoint)
+            {
+                case EndPoint.RSSFeeds:
+                    uriBuilder.Path += "RSSFeeds";
+                    break;
+            }
+
+            return uriBuilder.Uri;
+        }
+    }
+}
