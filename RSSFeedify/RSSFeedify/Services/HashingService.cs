122e6bb07a71415fbf7e0fb179f183e2e43b4da8
Marek Eibel <536376@mail.muni.cz>
2024-05-08 14:53:45 +0200

    feat(services): add hashing service

---
+++ RSSFeedify/RSSFeedify/Services/HashingService.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace RSSFeedify.Services
+{
+    public static class HashingService
+    {
+        public static string GetHash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                var builder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    builder.Append(data[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public static bool VerifyHash(string input, string hash)
+        {
+            string hashOfInput = GetHash(input);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(hashOfInput, hash) == 0;
+        }
+    }
+}
