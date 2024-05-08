73d778f4dcb0c2bf01abe8a3b09da1c33e8057d0
Marek Eibel <536376@mail.muni.cz>
2024-05-08 23:56:22 +0200

    feat(cli): implement http service

---
+++ RSSFeedify/RSSFeedifyCLIClient/Services/HTTPService.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+
+namespace RSSFeedifyCLIClient.Services
+{
+    public class HTTPService
+    {
+        private HttpClient _httpClient;
+
+        public HTTPService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<(bool success, HttpResponseMessage response)> Get(Uri uri)
+        {
+            using (_httpClient)
+            {
+                try
+                {
+                    HttpResponseMessage response = await _httpClient.GetAsync(uri.ToString());
+                    return (true, response);
+                }
+                catch (Exception ex)
+                {
+                    return (false, new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                    {
+                        Content = new StringContent($"Failed to fetch data from {uri}: {ex.Message}")
+                    });
+                }
+            }
+        }
+    }
+}
