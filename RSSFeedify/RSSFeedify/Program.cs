b58102f23a18297b1711c4cfc7db95816d8bf8a7
Marek Eibel <536376@mail.muni.cz>
2024-07-16 21:38:35 +0200

    feat: add auth into swagger to add jwt bearer

--- RSSFeedify/RSSFeedify/Program.cs
+++ RSSFeedify/RSSFeedify/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PostgreSQL.Data;
 using RSSFeedify.Models;
 using RSSFeedify.Repository;
@@ -73,7 +74,37 @@ builder.Services.AddScoped<IRSSFeedRepository>(serviceProvider =>
 
 // Add Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API Name", Version = "v1" });
+
+    // Define the security scheme (Bearer type)
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    });
+
+    // Make sure Swagger UI requires a Bearer token to be specified
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 // Register hosted service for polling of RSSFeeds.
 builder.Services.AddHostedService<RSSFeedPollingService>();
