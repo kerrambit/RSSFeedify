7bb698238bba9a3778fcab0093e86f716fbcb0c2
Marek Eibel <536376@mail.muni.cz>
2024-07-16 21:31:19 +0200

    feat: add new models for login and registration and user

---
+++ RSSFeedify/RSSFeedify/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace RSSFeedify.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public List<RSSFeed> WatchedList { get; set; }
+    }
+}
