fe0587e31622e3a29785de6cd3bc9ddb96e79cf1
Marek Eibel <536376@mail.muni.cz>
2024-07-17 21:59:57 +0200

    feat: implement roles seeder

---
+++ RSSFeedify/RSSFeedify/Services/Seeder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PostgreSQL.Data;
+using RSSFeedify.Models;
+
+namespace RSSFeedify.Services
+{
+    public class Seeder
+    {
+        public static async Task Run(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                string[] roles = ApplicationUserRoleExtensions.ConvertApplicationUserRoleExtensionsToStringArray();
+
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                foreach (string role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                }
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
