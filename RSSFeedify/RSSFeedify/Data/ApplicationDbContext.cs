c7011832d06aa749c65101026f61bef018c5e6af
Marek Eibel <536376@mail.muni.cz>
2024-07-18 21:59:14 +0200

    refactor: refactoring

--- RSSFeedify/RSSFeedify/Data/ApplicationDbContext.cs
+++ RSSFeedify/RSSFeedify/Data/ApplicationDbContext.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RSSFeedify.Models;
-using System.Reflection.Emit;
 
 namespace PostgreSQL.Data
 {
