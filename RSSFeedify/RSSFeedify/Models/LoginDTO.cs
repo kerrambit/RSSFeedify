30d1cb38fabfa7326c4942341a71dd8077958405
Marek Eibel <536376@mail.muni.cz>
2024-07-18 21:55:57 +0200

    feat: add modelstate validation for loginDTO

--- RSSFeedify/RSSFeedify/Models/LoginDTO.cs
+++ RSSFeedify/RSSFeedify/Models/LoginDTO.cs
@@ -1,8 +1,14 @@
-﻿namespace RSSFeedify.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RSSFeedify.Models
 {
     public class LoginDTO
     {
+        [Required]
+        [EmailAddress(ErrorMessage = "The email adress is not valid.")]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
