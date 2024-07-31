ba2bfce82b6b9ceacf7ccb23021edcbd84d2e333
Marek Eibel <536376@mail.muni.cz>
2024-07-31 21:35:11 +0200

    feat: add logout data model (https://github.com/kerrambit/RSSFeedify/issues/19)

---
+++ RSSFeedify/RSSFeedify/Models/LogoutDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RSSFeedify.Models
+{
+    public class LogoutDTO
+    {
+        [Required]
+        public string JWT {  get; set; }
+    }
+}
