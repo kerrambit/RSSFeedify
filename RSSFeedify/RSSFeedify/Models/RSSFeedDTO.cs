7cc3a06e01d61de98b4f15c5899b2c4c8b6219e2
Marek Eibel <536376@mail.muni.cz>
2024-08-01 18:12:51 +0200

    fix: validate polling time (https://github.com/kerrambit/RSSFeedify/issues/24)

--- RSSFeedify/RSSFeedify/Models/RSSFeedDTO.cs
+++ RSSFeedify/RSSFeedify/Models/RSSFeedDTO.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace RSSFeedify.Models
 {
     public class RSSFeedDTO
@@ -6,6 +8,9 @@ namespace RSSFeedify.Models
         public string Name { get; set; }
         public string? Description { get; set; }
         public Uri SourceUrl { get; set; }
+
+        [Range(typeof(double), "10.0", "60.0",
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public double PollingInterval { get; set; }
     }
 }
