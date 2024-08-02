454b6bdc553d4b54d867de4756e835b95fb7a391
Marek Eibel <536376@mail.muni.cz>
2024-08-02 22:53:49 +0200

    refactor: move error writer to the IO folder to make it accessible for AccountManager

---
+++ RSSFeedify/RSSFeedifyCLIClient/IO/ApplicationErrorWriter.cs
@@ -0,0 +1,50 @@
+﻿using CommandParsonaut.Interfaces;
+using RSSFeedifyClientCore;
+
+namespace RSSFeedifyCLIClient.IO
+{
+    public class ApplicationErrorWriter
+    {
+        private readonly IWriter _writer;
+
+        public ApplicationErrorWriter(IWriter writer)
+        {
+            _writer = writer;
+        }
+
+        public void RenderErrorMessage(ApplicationError error)
+        {
+            switch (error)
+            {
+                case ApplicationError.Network:
+                    _writer.RenderErrorMessage($"Unable to send the request!");
+                    break;
+                case ApplicationError.DataType:
+                    _writer.RenderErrorMessage("Wrong data type received!");
+                    break;
+                case ApplicationError.InvalidJsonFormat:
+                    _writer.RenderErrorMessage("Invalid Json format!");
+                    break;
+                case ApplicationError.General:
+                default:
+                    _writer.RenderErrorMessage("Error occured!");
+                    break;
+            }
+        }
+
+        public void RenderErrorMessage(string error)
+        {
+            _writer.RenderErrorMessage(error);
+        }
+
+        public void RenderErrorMessage()
+        {
+            RenderErrorMessage(ApplicationError.General);
+        }
+
+        public void RenderInvalidUrlWarningMessage(string link)
+        {
+            _writer.RenderWarningMessage($"Provided URL '{link}' does not seem to be valid URL and thus the link was refused to be opened in a browser.");
+        }
+    }
+}
