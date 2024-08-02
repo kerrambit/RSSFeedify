57fcafeb99601ac69697c319a9435363e30e506d
Marek Eibel <536376@mail.muni.cz>
2024-08-02 16:33:52 +0200

    feat: copied some classes from CLI Client project

---
+++ RSSFeedify/RSSFeedifyClientCore/JsonConvertor.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace RSSFeedifyClientCore
+{
+    public static class JsonConvertor
+    {
+        public static string ConvertObjectToJsonString(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
