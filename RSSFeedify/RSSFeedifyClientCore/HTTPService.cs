7d6d81600d0508dc0f6a7c20af206927a96f85fb
Marek Eibel <536376@mail.muni.cz>
2024-08-04 10:55:36 +0200

    feat: extend content types and add status code enums + retrieve methods (https://github.com/kerrambit/RSSFeedify/issues/32)

--- RSSFeedify/RSSFeedifyClientCore/HTTPService.cs
+++ RSSFeedify/RSSFeedifyClientCore/HTTPService.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using System.Text;
+﻿using System.Text;
 
 namespace RSSFeedifyClientCore
 {
@@ -9,13 +8,29 @@ namespace RSSFeedifyClientCore
 
         public enum ContentType
         {
+            Empty,
             AppJson,
             AppXml,
             AudioMpeg,
             ImgGif,
             TxtHtml,
+            TxtPlain,
             Unkown
         }
+
+        public enum StatusCode
+        {
+            OK = 200,
+            Created = 201,
+            BadRequest = 400,
+            Unauthorized = 401,
+            Forbidden = 403,
+            NotFound = 404,
+            MethodNotAllowed = 405,
+            InternalServerError = 500
+        }
+
+        public record HttpServiceResponseMessageMetaData(ContentType ContentType, StatusCode StatusCode);
 
         public HTTPService(HttpClient httpClient)
         {
@@ -132,12 +147,49 @@ namespace RSSFeedifyClientCore
 
         public static ContentType RetrieveContentType(HttpResponseMessage response)
         {
+            if (response.Content == null || response.Content.Headers.ContentType == null)
+            {
+                return ContentType.Empty;
+            }
+
             switch (response.Content.Headers.ContentType?.MediaType)
             {
                 case "application/json":
                     return ContentType.AppJson;
+                case "text/plain":
+                    return ContentType.TxtPlain;
                 default:
                     return ContentType.Unkown;
+            }
+        }
+
+        public static async Task<string> RetrieveAndStringifyContent(HttpResponseMessage response)
+        {
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public static StatusCode RetrieveStatusCode(HttpResponseMessage response)
+        {
+            switch (response.StatusCode)
+            {
+                case System.Net.HttpStatusCode.OK:
+                    return StatusCode.OK;
+                case System.Net.HttpStatusCode.BadRequest:
+                    return StatusCode.BadRequest;
+                case System.Net.HttpStatusCode.Forbidden:
+                    return StatusCode.Forbidden;
+                case System.Net.HttpStatusCode.Created:
+                    return StatusCode.Created;
+                case System.Net.HttpStatusCode.Unauthorized:
+                    return StatusCode.Unauthorized;
+                case System.Net.HttpStatusCode.InternalServerError:
+                    return StatusCode.InternalServerError;
+                case System.Net.HttpStatusCode.MethodNotAllowed:
+                    return StatusCode.MethodNotAllowed;
+                case System.Net.HttpStatusCode.NotFound:
+                    return StatusCode.NotFound;
+                default:
+                    return StatusCode.BadRequest;
             }
         }
 
