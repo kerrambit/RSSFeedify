a4b8febe6c7896fe656bc3fa11d6168f6b8fb354
Marek Eibel <536376@mail.muni.cz>
2024-08-04 10:57:28 +0200

    refactor: unify application errors messages

--- RSSFeedify/RSSFeedifyCLIClient/IO/ApplicationErrorWriter.cs
+++ RSSFeedify/RSSFeedifyCLIClient/IO/ApplicationErrorWriter.cs
@@ -11,67 +11,9 @@ namespace RSSFeedifyCLIClient.IO
         {
             _writer = writer;
         }
-
-        public void RenderErrorMessage(ApplicationError error)
+        public void RenderErrorMessage()
         {
-            switch (error)
-            {
-                case ApplicationError.Network:
-                    _writer.RenderErrorMessage($"Unable to send the request!");
-                    break;
-                case ApplicationError.DataType:
-                    _writer.RenderErrorMessage("Wrong data type received!");
-                    break;
-                case ApplicationError.InvalidJsonFormat:
-                    _writer.RenderErrorMessage("Invalid Json format!");
-                    break;
-                case ApplicationError.General:
-                default:
-                    _writer.RenderErrorMessage("Error occured!");
-                    break;
-            }
-        }
-
-        public void RenderErrorMessage(DetailedApplicationError error)
-        {
-            var details = error.Details;
-
-            switch (error.Error)
-            {
-                case ApplicationError.Network:
-                    _writer.RenderErrorMessage($"Unable to send the request! Detailed message: {details}");
-                    break;
-                case ApplicationError.DataType:
-                    _writer.RenderErrorMessage($"Wrong data type received! Detailed message: {details}");
-                    break;
-                case ApplicationError.InvalidJsonFormat:
-                    _writer.RenderErrorMessage($"Invalid Json format! Detailed message: {details}");
-                    break;
-                case ApplicationError.General:
-                default:
-                    _writer.RenderErrorMessage($"Error occured! Detailed message: {details}");
-                    break;
-            }
-        }
-
-        public void RenderErrorMessage(ApplicationError error, string details)
-        {
-            switch (error)
-            {
-                case ApplicationError.Network:
-                    _writer.RenderErrorMessage($"Unable to send the request! Detailed message: {details}");
-                    break;
-                case ApplicationError.DataType:
-                    _writer.RenderErrorMessage($"Wrong data type received! Detailed message: {details}");
-                    break;
-                case ApplicationError.InvalidJsonFormat:
-                    _writer.RenderErrorMessage($"Invalid Json format! Detailed message: {details}");
-                    break;
-                case ApplicationError.General:
-                default:
-                    _writer.RenderErrorMessage($"Error occured! Detailed message: {details}");
-                    break;
-            }
+            RenderErrorMessage(ApplicationError.General);
         }
 
         public void RenderErrorMessage(string error)
@@ -79,14 +21,63 @@ namespace RSSFeedifyCLIClient.IO
             _writer.RenderErrorMessage(error);
         }
 
-        public void RenderErrorMessage()
+        public void RenderErrorMessage(ApplicationError error, string details)
         {
-            RenderErrorMessage(ApplicationError.General);
+            _writer.RenderErrorMessage(CreateErrorMessage(error, details));
         }
 
+        public void RenderErrorMessage(ApplicationError error)
+        {
+            _writer.RenderErrorMessage(CreateErrorMessage(error));
+        }
+
+        public void RenderErrorMessage(DetailedApplicationError error)
+        {
+            _writer.RenderErrorMessage(CreateErrorMessage(error.Error, error.Details));
+        }
+
+        public void RenderErrorMessage(DetailedApplicationError error, string? overrideDetails = null)
+        {
+            var details = error.Details;
+            if (!error.HasDetails() && overrideDetails is not null)
+            {
+                details = overrideDetails;
+            }
+
+            _writer.RenderErrorMessage(CreateErrorMessage(error.Error, details));
+        }
+
+        // TODO: should be replaces by DetailedApplicationError
         public void RenderInvalidUrlWarningMessage(string link)
         {
             _writer.RenderWarningMessage($"Provided URL '{link}' does not seem to be valid URL and thus the link was refused to be opened in a browser.");
+        }
+
+        private string CreateErrorMessage(ApplicationError error, string? details = null)
+        {
+            string message = error switch
+            {
+                ApplicationError.General => "Error occurred!",
+                ApplicationError.NetworkGeneral => "Unable to send the request!",
+                ApplicationError.NetworkUnexpectedStatusCode => "Unexpected status code received!",
+                ApplicationError.NetworkBadRequest => "Bad request!",
+                ApplicationError.NetworkForbidden => "Access forbidden!",
+                ApplicationError.NetworkNotFound => "Not found!",
+                ApplicationError.NetworkUnauthorized => "Unauthorized access!",
+                ApplicationError.NetworkNotSupported => "Operation not supported!",
+                ApplicationError.NetworkServerError => "Server error!",
+                ApplicationError.NetworkUnexpectedDataType => "Wrong data type received!",
+                ApplicationError.InvalidJsonFormat => "Invalid Json format!",
+                ApplicationError.UserNotLoggedIn => "You are not logged in!",
+                _ => "Error occurred!"
+            };
+
+            if (!string.IsNullOrEmpty(details))
+            {
+                message += $" Detailed message: '{details}'.";
+            }
+
+            return message;
         }
     }
 }
