43f9216bf0564ff0162a4c60b81985da0fd13c39
Marek Eibel <536376@mail.muni.cz>
2024-08-20 15:06:20 +0200

    feat: add two utils class into clientCore - env vars loader and config directory serivice to load .env and other config files
    todo: we have to sort out where to save the config files, now service reads them inside the cloned solution in the projects, that will have to be probably changed

---
+++ RSSFeedify/RSSFeedifyClientCore/Services/ConfigDirectoryService.cs
@@ -0,0 +1,56 @@
+﻿using RSSFeedifyCommon.Types;
+
+namespace RSSFeedifyClientCore.Services
+{
+    public static class ConfigDirectoryService
+    {
+        public static Result<string, string> GetConfigFilesDirectory()
+        {
+            string configFilesDirectory = string.Empty;
+            try
+            {
+                configFilesDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            }
+            catch (Exception e) when(e is IOException || e is UnauthorizedAccessException || e is ArgumentException || e is PathTooLongException || e is FileNotFoundException || e is DirectoryNotFoundException || e is NotSupportedException || e is System.Security.SecurityException)
+            {
+                return Result.Error<string, string>($"Exception occured when loading environment variable(s): '{e.Message}'.");
+            }
+
+            return Result.Ok<string, string>(configFilesDirectory);
+        }
+
+        public static Result<string, string> GetEnvironmentFilePath()
+        {
+            var configFilesDirectoryResult = GetConfigFilesDirectory();
+            if (configFilesDirectoryResult.IsSuccess)
+            {
+                string envFilePath = string.Empty;
+                try
+                {
+                    envFilePath = Path.Combine(configFilesDirectoryResult.GetValue, ".env");
+                    return Result.Ok<string, string>(envFilePath);
+                }
+                catch (Exception e) when (e is ArgumentException || e is ArgumentNullException)
+                {
+                    return Result.Error<string, string>($"Exception occured when loading environment variable(s): '{e.Message}'.");
+                }
+            }
+
+            return configFilesDirectoryResult;
+        }
+
+        public static Result<string, string> GetEnvironmentFilePath(string basePath)
+        {
+            string envFilePath = string.Empty;
+            try
+            {
+                envFilePath = Path.Combine(basePath, ".env");
+                return Result.Ok<string, string>(envFilePath);
+            }
+            catch (Exception e) when (e is ArgumentException || e is ArgumentNullException)
+            {
+                return Result.Error<string, string>($"Exception occured when loading the .env file: '{e.Message}'.");
+            }
+        }
+    }
+}
